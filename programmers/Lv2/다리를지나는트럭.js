88e631fc787c0884fa391928e972154239185e5b
Suhwa <hansuhwa2005@gmail.com>
2021-09-06 14:19:57 +0900

    불필요한 로직 수정 필요

---
+++ programmers/Lv2/다리를지나는트럭.js
@@ -0,0 +1,41 @@
+/*
+1. 문제 링크: https://programmers.co.kr/learn/courses/30/lessons/42583 
+2. 접근방법 
+- 케이스 분류 
+  1) default 케이스: 다리에 있는 트럭 이동, 다리를 다 건넌 트럭 빼기 
+  2) 다리에 자리, 여유가 있을 경우: 대기하고 있는 트럭 넣어주기 
+*/
+
+// 처음 짠 코드 
+// : 작동과정을 하나하나 구현해, 다리가 길 때 시간이 오래 걸림. 
+function solution(bridge_length, weight, truck_weights) {
+    let timer = 0;
+    let currentBriage = [];
+    while (currentBriage.length || truck_weights.length) {
+        // default : 다리에 있는 트럭, 이동 & 다온 트럭, shift 
+        if (currentBriage.length !== 0) {
+            currentBriage = currentBriage.map((truck) => ({ ...truck, 'moving': truck.moving + 1 }));
+            if (currentBriage[0].moving === bridge_length) { currentBriage.shift(); }
+        }
+        // 자리, 무게 여유가 있다 ? 다음 트럭 넣어주기 
+        if (truck_weights.length && currentBriage.length < bridge_length) {
+            const temp = weight - sum(currentBriage.map((v) => v.weight));
+            const nextTruckWeight = truck_weights[0];
+            if (temp >= nextTruckWeight) {
+                currentBriage.push({
+                    weight: truck_weights.shift(),
+                    moving: 0,
+                });
+            }
+        }
+        timer++;
+    }
+    return timer;
+}
+
+function sum(arr) {
+    return arr.reduce((res, cur) => res + cur, 0);
+}
+
+// 테스트 코드 
+solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]); // 110
