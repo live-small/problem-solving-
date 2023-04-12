2d979378b688d8fd601322ca78e099e4a14a1174
suhwa <hansuhwa2005@gmail.com>
2023-04-12 18:42:09 +0900

    BFS: 2178

--- baekjoon/2178.js
+++ baekjoon/2178.js
@@ -1,8 +1,8 @@
-let fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = fs.readFileSync(filePath).toString().split("\n");
-const [N, M] = input.shift().split(" ");
-const board = [...input].map((r) => r.split("").map(Number));
+let fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().split('\n');
+const [N, M] = input.shift().split(' ');
+const board = [...input].map((r) => r.split('').map(Number));
 
 /*
 -조건
@@ -21,21 +21,60 @@ const dx = [1, 0, -1, 0];
 
 const queue = [[0, 0]];
 
+// bfs - 인접노드따라 탐색
 while (queue.length) {
-	const [y, x] = queue.shift();
+  const [y, x] = queue.shift();
 
-	for (let i = 0; i < 4; i++) {
-		const ny = y + dy[i];
-		const nx = x + dx[i];
+  if (y === N - 1 && x === M - 1) {
+    console.log(board[y][x]);
+    return;
+  }
 
-		// 격자 안인지 체크
-		if (ny < N && ny >= 0 && nx < M && nx >= 0) {
-			if (board[ny][nx] === 1) {
-				// 첫 방문 && 이동가능 칸
-				board[ny][nx] = board[y][x] + 1;
-				queue.push([ny, nx]);
-			}
-		}
-	}
+  for (let i = 0; i < 4; i++) {
+    const ny = y + dy[i];
+    const nx = x + dx[i];
+
+    // 격자 안인지 체크
+    if (ny < N && ny >= 0 && nx < M && nx >= 0) {
+      if (board[ny][nx] === 1) {
+        // 첫 방문 && 이동가능 칸 -> 이렇게해도 괜찮은게, 가장 빨리가는 루트가 목적지에 방문처리할거임
+        board[ny][nx] = board[y][x] + 1;
+        queue.push([ny, nx]);
+      }
+    }
+  }
 }
-console.log(board[N - 1][M - 1]);
+
+// 프로그래머스 - 게임맵 최단거리 풀이
+{
+  const DIRECTION = [
+    [-1, 0],
+    [1, 0],
+    [0, 1],
+    [0, -1],
+  ];
+
+  // BFS
+  function solution(maps) {
+    const r = maps.length - 1;
+    const c = maps[0].length - 1;
+    const queue = [[0, 0]];
+
+    while (queue.length) {
+      const [y, x] = queue.shift();
+      if (y === r && x === c) return maps[y][x];
+
+      for (let k = 0; k < 4; k++) {
+        const [dy, dx] = DIRECTION[k];
+        const ny = y + dy;
+        const nx = x + dx;
+
+        if (ny >= 0 && ny <= r && nx >= 0 && nx <= c && maps[ny][nx] === 1) {
+          maps[ny][nx] = maps[y][x] + 1;
+          queue.push([ny, nx]);
+        }
+      }
+    }
+    return -1;
+  }
+}
